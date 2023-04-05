e06ae53201ec17233fd0c32df60a12047be48151
dzaslavskiy <dzaslavskiy@users.noreply.github.com>
2023-04-05 04:31:18 -0400

    Add delete endpoint

--- gdrive/api.py
+++ gdrive/api.py
@@ -7,6 +7,8 @@ import io
 import logging
 
 import fastapi
+from fastapi import Response, status
+from googleapiclient.http import HttpError
 from starlette.requests import Request
 
 from . import client, settings
@@ -15,19 +17,44 @@ log = logging.getLogger(__name__)
 
 router = fastapi.APIRouter()
 
+client.init()
 
 @router.post("/upload")
-async def upload_file(id, filename, request: Request, base64: bool = False):
+async def upload_file(id, filename, request: Request, response: Response, base64: bool = False):
     """
     Upload file to gdrive.
     """
-    body = await request.body()
 
-    if base64:
-        body = base64decoder.b64decode(body)
+    try:
+        body = await request.body()
 
-    stream = io.BytesIO(body)
+        if base64:
+            body = base64decoder.b64decode(body)
 
-    parent = client.create_folder(id, settings.ROOT_DIRECTORY)
-    if parent != None:
+        stream = io.BytesIO(body)
+
+        parent = client.create_folder(id, settings.ROOT_DIRECTORY)
         client.upload_basic(filename, parent, stream)
+
+    except HttpError as error:
+        log.error(f"An error occurred: {error}")
+        response.status_code = error.status_code
+
+@router.delete("/upload")
+async def delete_file(filename, response: Response):
+    """
+    Delete file from gdrive.
+    """
+
+    try:
+        files = client.get_files(filename)
+        if files:
+            for file in files:
+                client.delete_file(file['id'])
+        else:
+            response.status_code = status.HTTP_404_NOT_FOUND
+
+    except HttpError as error:
+        log.error(f"An error occurred: {error}")
+        response.status_code = error.status_code
+         
--- gdrive/settings.py
+++ gdrive/settings.py
@@ -16,17 +16,26 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", logging.getLevelName(logging.INFO))
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 SERVICE_ACCOUNT_FILE = "credentials.json"
-ROOT_DIRECTORY = None
+ROOT_DIRECTORY = ""
 CREDENTIALS = None
 
-vcap_services = os.getenv("VCAP_SERVICES")
-if vcap_services is not None:
-    try:
+
+try:
+    vcap_services = os.getenv("VCAP_SERVICES")
+    config = {}
+    if vcap_services:
         user_services = json.loads(vcap_services)["user-provided"]
         for service in user_services:
             if service["name"] == "gdrive":
-                CREDENTIALS = service["credentials"]["credentials"]
-                ROOT_DIRECTORY = service["credentials"]["root_directory"]
-    except (json.JSONDecodeError, KeyError) as err:
-        log.warning("Unable to load credentials from VCAP_SERVICES")
-        log.debug("Error: %s", str(err))
+                log.info("Loading credentials from env var")
+                config = service["credentials"]
+                break
+    else:
+        with open(SERVICE_ACCOUNT_FILE) as file:
+            log.info("Loading credentials from creds file")
+            config = json.load(file)
+    CREDENTIALS = config["credentials"]
+    ROOT_DIRECTORY = config["root_directory"]
+except (json.JSONDecodeError, KeyError) as err:
+    log.warning("Unable to load credentials from VCAP_SERVICES")
+    log.debug("Error: %s", str(err))

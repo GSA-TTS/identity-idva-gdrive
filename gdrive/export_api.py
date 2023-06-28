93026ab02a938b0d5811e4e04933daaf4dda6177
shumatepf <shumatepfs@gmail.com>
2023-06-28 11:56:32 -0400

    Run survey export as a background task

--- gdrive/export_api.py
+++ gdrive/export_api.py
@@ -7,10 +7,8 @@ import json
 import logging
 
 import fastapi
-from pydantic import BaseModel, typing
-from fastapi import Response, status, HTTPException
-from googleapiclient.http import HttpError
-from starlette.requests import Request
+from pydantic import BaseModel
+from fastapi import BackgroundTasks, responses
 
 from gdrive import export_client, client, settings, error
 
@@ -35,24 +33,34 @@ class SurveyResponseModel(BaseModel):
 
 
 @router.post("/survey-export")
-async def survey_upload_response(request: SurveyResponseModel):
+async def survey_upload_response(
+    request: SurveyResponseModel, background_tasks: BackgroundTasks
+):
     """
-    Single endpoint to handle qualtrics response fetching and exporting
+    Single endpoint that kicks off qualtrics response fetching and exporting
     """
 
+    background_tasks.add_task(
+        survey_upload_response_task, request.responseId, request.surveyId
+    )
+
+    return responses.JSONResponse(
+        status_code=202, content=f"Response {request.responseId} is being processed."
+    )
+
+
+async def survey_upload_response_task(responseId, surveyId):
+    """
+    Background task that handles qualtrics response fetching and exporting
+    """
     try:
-        # call function that invokes qualtrics microservice to get response
-        response = export_client.get_qualtrics_response(
-            request.surveyId, request.responseId
-        )
+        response = export_client.get_qualtrics_response(surveyId, responseId)
 
         # call function that queries ES for all analytics entries (flow interactionId) with responseId
-        interactionIds = export_client.export_response(request.responseId, response)
+        interactionIds = export_client.export_response(responseId, response)
 
         # export list of interactionIds to gdrive
         for id in interactionIds:
             await upload_file(id)
-
-        return response
     except error.ExportError as e:
-        raise HTTPException(status_code=400, detail=e.args)
+        log.error(e.args)

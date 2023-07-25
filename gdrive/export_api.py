50935131b8904fd7eb3b3582054a00692cb0e399
Peter Shumate <pshumate@easydynamics.com>
2023-07-25 10:52:03 -0400

    Make optional none type if it doesn't exist (#41)
    * Make optional none type if it doesn't exist
    
    * Remove Optional type

--- gdrive/export_api.py
+++ gdrive/export_api.py
@@ -9,7 +9,6 @@ import logging
 import fastapi
 from pydantic import BaseModel
 from fastapi import BackgroundTasks, responses
-from typing import Optional
 
 from gdrive import export_client, client, settings, error
 
@@ -42,7 +41,7 @@ class SurveyParticipantModel(BaseModel):
 
     surveyId: str
     responseId: str
-    participant: Optional[ParticipantModel]
+    participant: ParticipantModel | None = ...
 
 
 @router.post("/survey-export")
